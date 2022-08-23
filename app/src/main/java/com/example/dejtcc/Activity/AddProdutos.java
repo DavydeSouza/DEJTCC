3a90c391ba8d4a9ec283dbe4f58ec0397d97115a
Davy de Souza <davy.desouza02@gmail.com>
2022-08-23 17:00:23 -0300

    5- Tristeza infinita:(

--- app/src/main/java/com/example/dejtcc/Activity/AddProdutos.java
+++ app/src/main/java/com/example/dejtcc/Activity/AddProdutos.java
@@ -2,15 +2,34 @@ package com.example.dejtcc.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.dejtcc.MainActivity;
 import com.example.dejtcc.R;
 
 public class AddProdutos extends AppCompatActivity {
+
+    Button voltar, add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_produtos);
+        iniciarComponetes();
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(AddProdutos.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void iniciarComponetes(){
+        voltar = findViewById(R.id.voltar);
+        add = findViewById(R.id.addItem);
     }
 }
