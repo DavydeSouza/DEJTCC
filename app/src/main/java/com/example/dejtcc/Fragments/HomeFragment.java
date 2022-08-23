8ff60cde5bb62be4020acf29566565464f44f8f8
Davy de Souza <davy.desouza02@gmail.com>
2022-08-23 16:31:35 -0300

    5- Tristeza infinita:(

--- app/src/main/java/com/example/dejtcc/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/dejtcc/Fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.dejtcc.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.dejtcc.Activity.AddProdutos;
 import com.example.dejtcc.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
 
     FloatingActionButton floatingActionButton;
+    View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,15 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        floatingActionButton = view.findViewById(R.id.fbADD);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), AddProdutos.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 }
