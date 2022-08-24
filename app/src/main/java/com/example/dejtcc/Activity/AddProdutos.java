cc3e49a974fdd5b79757e3c9d52b9933890c8b84
Davy de Souza <davy.desouza02@gmail.com>
2022-08-24 15:25:15 -0300

    6- Add produtos :(

--- app/src/main/java/com/example/dejtcc/Activity/AddProdutos.java
+++ app/src/main/java/com/example/dejtcc/Activity/AddProdutos.java
@@ -1,18 +1,32 @@
 package com.example.dejtcc.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.dejtcc.MainActivity;
 import com.example.dejtcc.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddProdutos extends AppCompatActivity {
 
+
+    EditText comida,tipo,descricao,preco,estoque;
     Button voltar, add;
+    String produtoID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +40,56 @@ public class AddProdutos extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                salvarProdutos();
+            }
+        });
     }
 
     public void iniciarComponetes(){
         voltar = findViewById(R.id.voltar);
         add = findViewById(R.id.addItem);
+        comida = findViewById(R.id.eats);
+        tipo = findViewById(R.id.type);
+        descricao = findViewById(R.id.description);
+        preco = findViewById(R.id.price);
+        estoque = findViewById(R.id.inventory);
+    }
+    private void salvarProdutos(){
+        String UserID;
+        String eats  =comida.getText().toString();
+        String type =tipo.getText().toString();
+        String description =descricao.getText().toString();
+        String price =preco.getText().toString();
+        String inventory =estoque.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String,Object> produtos = new HashMap<>();
+        produtos.put("comida",eats);
+        produtos.put("tipo",type);
+        produtos.put("descricao",description);
+        produtos.put("preco",price);
+        produtos.put("estoque",inventory);
+
+        produtoID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("produtos").document(produtoID);
+
+        documentReference.set(produtos).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("db","Sucesso ao Adicionar");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db_erro","Falha ao Salvar" + e.toString());
+
+            }
+        });
+
     }
 }
